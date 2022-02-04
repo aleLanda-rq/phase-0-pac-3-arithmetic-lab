bc6695a73c20112354f141252a23b7c4e71ea5d8
Alejandra Landa <aleriquene@gmail.com>
2022-02-04 12:52:18 -0600

    commit tree fa2f490e0e19f47fe818ea6be28b7319ee841fed

---
+++ index.js
@@ -0,0 +1,45 @@
+function makeInt(n) {
+    var n = 10
+    var numberTwo=11
+    n++; 
+    n--;
+    n*2;
+parseInt("2", 10); //=> 2
+parseInt("2.2222", 10);
+parseInt("nonsense!", 10);
+}
+function preserveDecimal(string) {
+parseFloat("10.567889"); 
+}
+function subtract(a,b){
+    return (a-b);
+
+}
+function add(a,b) {
+return(a+b)
+  }
+function multiply(a,b) {
+return (a*b)
+}
+function divide(a,b) {
+return (a/b)
+}
+function add(a, b) {
+return (a+b)
+}
+function increment(a){
+ return(a+1)
+}
+function decrement(a){
+    return(a-1)
+}
+function makeInt(a){
+    return parseInt(a,10)
+}
+function preserveDecimal(a){
+return parseInt("nonsense!");
+}
+function preserveDecimal(a){
+ return parseFloat(a,2.222);
+}
+
